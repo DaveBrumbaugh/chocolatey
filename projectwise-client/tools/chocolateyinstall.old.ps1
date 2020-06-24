e450a46cf350c9119eac4cd06d9662a6d2c23a04
DaveBrumbaugh <Dave.brumbaugh@bentley.com>
2020-06-24 09:36:24 -0400

    First release

---
+++ projectwise-client/tools/chocolateyinstall.old.ps1
@@ -0,0 +1,30 @@
+﻿[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$AdminKey = “HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}”
+Set-ItemProperty -Path $AdminKey -Name “IsInstalled” -Value 0
+$UserKey = “HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}”
+Set-ItemProperty -Path $UserKey -Name “IsInstalled” -Value 0
+Stop-Process -Name Explorer
+Write-Host “IE Enhanced Security Configuration (ESC) has been disabled.” -ForegroundColor Green
+
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://pwstorageazure.blob.core.windows.net/public/pwclt100003329en.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+
+  softwareName  = 'projectwise-client*'
+
+  checksum      = '55AA2A0D2178FD51FFF274C399D0D1AE602FC1A359F587E159FE32C3FE0F27DF'
+  checksumType  = 'sha256'
+
+  validExitCodes= @(0, 3010, 1641)
+  silentArgs   = '/s /v"/qn"'
+}
+
+Install-ChocolateyPackage @packageArgs
+
+Write-Host “Install-ChocolateyPackage complete.” -ForegroundColor Green
---
+++ projectwise-client/tools/chocolateyinstall.ps1
@@ -0,0 +1,24 @@
+﻿[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://pwstorageazure.blob.core.windows.net/public/pwclt100003329en.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+
+  softwareName  = 'projectwise-client*'
+
+  checksum      = '55AA2A0D2178FD51FFF274C399D0D1AE602FC1A359F587E159FE32C3FE0F27DF'
+  checksumType  = 'sha256'
+
+  validExitCodes= @(0, 3010, 1641)
+  silentArgs   = '/s /v"/qn"'
+}
+
+Install-ChocolateyPackage @packageArgs
+
+Write-Host 'Install-ChocolateyPackage complete.' -ForegroundColor Green
