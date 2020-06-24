8a068d89ae20f6bc379018689dc71ad8e5056fe2
DaveBrumbaugh <Dave.brumbaugh@bentley.com>
2020-06-24 10:04:26 -0400

    Initial upload

---
+++ projectwise-deliverables-management/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://pwstorageazure.blob.core.windows.net/public/dmc10000581en.msi'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI'
+  url           = $url
+
+  softwareName  = 'projectwise-deliverables-management*'
+
+  checksum      = 'E6F0248EF7722E3CA3205494DADAAF5B8B7B19BDC41C968E6BA92548A379F44C'
+  checksumType  = 'sha256'
+
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
